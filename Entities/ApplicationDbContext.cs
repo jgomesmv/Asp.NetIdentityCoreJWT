c6bf9bca1ad252cffc908bff15f823db89210ee2
José Gomes <jose.gomes@egplusww.com>
2018-01-05 18:01:53 +0000

    AddWebApiSecuritySolution

---
+++ Entities/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Entities
+{
+    //public class ApplicationDbContext : IdentityDbContext
+    //{
+    //    public ApplicationDbContext()
+    //    {
+    //    }
+    //}
+}
